210b1bf774bddf3658432bb1b33dac46fc69cfa6
joel kaudzu <joelkaudzu9@example.com>
2026-08-16 13:58:36 +0200

    t

--- public/OneSignalSDKWorker.js
+++
@@ -1,5 +0,0 @@
-// public/OneSignalSDKWorker.js
-// OneSignal Service Worker
-// DO NOT EDIT - This file is required for push notifications
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
